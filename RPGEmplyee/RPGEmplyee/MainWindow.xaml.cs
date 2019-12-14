b069b2435a2696cf546b3a7356baa99becd0381b
Yecki <jens.bellmann1@web.de>
2019-12-14 22:43:00 +0100

    WASD movement (if not quite optimal but at this point I have to cut corners and the snappiness of the character takes less priority right now)

--- RPGEmplyee/RPGEmplyee/MainWindow.xaml.cs
+++ RPGEmplyee/RPGEmplyee/MainWindow.xaml.cs
@@ -27,6 +27,12 @@ namespace RPGEmployee
         public String gameState = "Menu";
 
         SoundPlayer menuBlip = new SoundPlayer(RPGEmployee.Properties.Resources.MenuBlip);
+        double[] characterDistanceToTheBorders = { 3, 2, 3, 2};
+        double movementSpeed = 2d;
+        Boolean charAnim = false;
+
+        long time = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        
 
         public MainWindow()
         {
@@ -39,6 +45,11 @@ namespace RPGEmployee
             if (newGamestate.Equals("Menu"))
             {
                 GameBackground.Visibility = SetVisibility(false);
+                Character.Visibility = SetVisibility(false);
+                CharacterAnim.Visibility = SetVisibility(false);
+                Crate.Visibility = SetVisibility(false);
+                Customer.Visibility = SetVisibility(false);
+
                 MenuBackground.Visibility = SetVisibility(true);
                 StartGame.Visibility = SetVisibility(true);
                 StartGameText.Visibility = SetVisibility(true);
@@ -47,9 +58,14 @@ namespace RPGEmployee
                 CloseBut.Visibility = SetVisibility(true);
                 CloseText.Visibility = SetVisibility(true);
                 gameState = "Menu";
-            }else if (newGamestate.Equals("Game"))
+            } else if (newGamestate.Equals("Game"))
             {
                 GameBackground.Visibility = SetVisibility(true);
+                Character.Visibility = SetVisibility(!charAnim);
+                CharacterAnim.Visibility = SetVisibility(charAnim);
+                Crate.Visibility = SetVisibility(true);
+                Customer.Visibility = SetVisibility(true);
+
                 MenuBackground.Visibility = SetVisibility(false);
                 StartGame.Visibility = SetVisibility(false);
                 StartGameHighlight.Visibility = SetVisibility(false);
@@ -68,6 +84,28 @@ namespace RPGEmployee
             else return System.Windows.Visibility.Hidden;
         }
 
+        public void MoveImage(Image[] toMove, int speedRight, int speedDown)
+        { if ((speedRight <= 1 || speedRight >= -1) && (speedDown <= 1 || speedDown >= -1)){
+
+                foreach (Image pic in toMove)
+                {
+                    double left = pic.Margin.Left + (movementSpeed * speedRight);
+                    double up = pic.Margin.Top + (movementSpeed * speedDown);
+                    double right = pic.Margin.Right - (movementSpeed * speedRight);
+                    double down = pic.Margin.Bottom - (movementSpeed * speedDown);
+
+                    pic.Margin = new Thickness(left, up, right, down);
+                    if (pic.Visibility.Equals(System.Windows.Visibility.Visible))
+                    {
+                        pic.Visibility = SetVisibility(false);
+                    } else
+                    {
+                        pic.Visibility = SetVisibility(true);
+                    }
+                }
+            }
+        }
+
         private void Window_MouseEnter(object sender, MouseEventArgs e)
         {
             switch (gameState)
@@ -79,24 +117,25 @@ namespace RPGEmployee
                             StartGameHighlight.Visibility = System.Windows.Visibility.Visible;
                             menuBlip.Play();
                         }
-                        
+
                         // if (e.Source.Equals(SettingText)) SettingHighlight.Visibility = System.Windows.Visibility.Visible;
                         if (e.Source.Equals(CloseText))
                         {
                             CloseHighlight.Visibility = System.Windows.Visibility.Visible;
                             menuBlip.Play();
                         }
-                        
-                    }break;
 
-                case "Game": { }break; 
+                    }
+                    break;
+
+                case "Game": { } break;
 
             }
-            
-             
-           
-            
-           
+
+
+
+
+
 
         }
 
@@ -116,7 +155,7 @@ namespace RPGEmployee
 
             }
 
-            
+
         }
 
         private void Window_MouseUp(object sender, MouseButtonEventArgs e)
@@ -138,30 +177,77 @@ namespace RPGEmployee
 
             }
 
-            
+
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
-        { }
+        {
+            switch (gameState)
+            {
+                case "Menu":
+                    {
+                        if (e.Key.Equals(Key.Escape)) Close();
+                    }
+                    break;
+
+                case "Game":
+                    {
+                        switch (e.Key)
+                        {
+                            case Key.A:
+                                {
+                                    Image[] player = { Character, CharacterAnim };
+                                    MoveImage(player,-1,0);
+                                }
+                                break;
+                            case Key.D:
+                                {
+                                    Image[] player = { Character, CharacterAnim };
+                                    MoveImage(player, 1, 0);
+                                }
+                                break;
+                            case Key.W:
+                                {
+                                    Image[] player = { Character, CharacterAnim };
+                                    MoveImage(player, 0, -1);
+                                }
+                                break;
+                            case Key.S:
+                                {
+                                    Image[] player = { Character, CharacterAnim };
+                                    MoveImage(player, 0, 1);
+                                }
+                                break;
+                        }
+                        
+                    }
+                    break;
+
+            }
+            
+        }
 
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
-                switch (gameState)
-                {
-                    case "Menu":
-                        {
-                            if (e.Key.Equals(Key.Escape)) Close();
-                        }
-                        break;
+            switch (gameState)
+            {
+                case "Menu":
+                    {
+                        if (e.Key.Equals(Key.Escape)) Close();
+                    }
+                    break;
 
-                    case "Game":
-                        {
-                            if(e.Key.Equals(Key.Escape)) SwitchGameState("Menu");
-                        }
-                        break;
+                case "Game":
+                    {
+                        if (e.Key.Equals(Key.Escape)) SwitchGameState("Menu");
+                    }
+                    break;
 
-                }
+            }
             
+
         }
+
     }
 }
+
